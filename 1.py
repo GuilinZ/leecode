be39e6ea1dabe5029282b8c87c30f3bd129b3b8d
GuilinZ <davezhang000@gmail.com>
2020-09-15 21:35:44 -0400

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,16 @@
+#1
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        pool={}
+        for idx,item in enumerate(nums):
+            residual = target - item
+            if residual in pool:
+                res_idx = pool[residual]
+                return [idx,res_idx]
+            else:
+                pool[item]=idx
